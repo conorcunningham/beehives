8ca5eb5439fd0156a044d14ba0ffe9558840a602
Conor Cunningham <conor.cunningham@gmail.com>
2026-05-30 11:19:46 +0200

    fix prisma instantiation code

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,7 +6,11 @@ if (!process.env.DATABASE_URL){
  throw new Error ("DATABASE_URL is not set as an env var.")
 }
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg ({ connectionString });
-const prisma = new PrismaClient({ adapter })
-export { prisma };
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
